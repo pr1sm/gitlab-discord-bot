53660545a40e67212497234a4d2c22a530e10847
Srinivas Dhanwada <dhanwads@gmail.com>
2018-04-01 03:03:30 -0500

    begin adding discord hook

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const request = require('request');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -10,7 +12,23 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get('/webhook', function(req, res) {
+
+    var forwardData = {
+        content: "gitlab sent a message!",
+        username: "custom-username",
+        avatar_url: `${req.headers.host}/images/gitlab-logo.png`,
+        tts: false
+    }
+
+    console.log('Forward Data:');
+    console.log(forwardData);
+
+    res.status(200);
     res.send('Hello World!');
+});
+
+app.get('/images/gitlab-logo.png', function(req, res) {
+    res.sendFile(path.join(__dirname, 'images', 'gitlab-logo.png'));
 });
 
 app.post('/webhook', function(req, res) {
@@ -18,6 +36,17 @@ app.post('/webhook', function(req, res) {
     console.log(req.headers);
     console.log('Body: ');
     console.log(req.body);
+
+    var forwardData = {
+        content: "gitlab sent a message!",
+        username: "custom-username",
+        avatar_url: `${req.headers.host}/images/gitlab-logo.png`,
+        tts: false
+    }
+
+    console.log('Forward Data:');
+    console.log(forwardData);
+
     res.status(200);
     res.send('OK');
 });
