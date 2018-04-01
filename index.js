1b02ce7f36156db50f5ef53de9ddebc375789f8e
Srinivas Dhanwada <dhanwads@gmail.com>
2018-04-01 03:18:57 -0500

    test discord webhook

--- index.js
+++ index.js
@@ -47,6 +47,16 @@ app.post('/webhook', function(req, res) {
     console.log('Forward Data:');
     console.log(forwardData);
 
+    request({
+        url: 'https://discordapp.com/api/webhooks/429916549017763841/-3yqpx7q_5WQV2WmLRJD50HaE5KblxZH7nU93PsTw3PMPQ6kmV0i2bIFOT0bHOsjQDMz',
+        method: 'POST',
+        json: true,
+        body: forwardData
+    }, function(error, resp, body) {
+        console.log('Discord Response');
+        console.log(resp);
+    })
+
     res.status(200);
     res.send('OK');
 });
